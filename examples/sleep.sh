24239666a71274045ab48eb4feab7ca2b1c3bb16
Jason Miller <jason.miller@inl.gov>
2015-10-19 14:39:39 -0600

    include a symbolic links directory, so PBS emulator binaries can be more easily enabled

--- examples/sleep.sh
+++ examples/sleep.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
-#
+#!/bin/sh
+# The following exmaple demonstrates possible options when using SGE or PBS
+
+#Active comments for PBS
+#PBS -N sleep_10
+#PBS -V
+#PBS -o sleep
+#PBS -e sleep
+#PBS -m bea
+#PBS -M testemail@somelocation.com
+#PBS -l nodes=1:ppn=1
+
+#Active comments for SGE
+#$ -N sleep_10
 #$ -cwd
-#$ -j y
-#$ -S /bin/bash
-#
+#$ -V
+#$ -o sleep
+#$ -e sleep
+#$ -m bea
+#$ -M testemail@somelocation.com
+
 date
 sleep 10
 date
